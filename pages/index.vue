5581d6bd75f4e2afaf2cde8e3fa1e6b5865c6b74
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-05 17:46:22 +0330

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,7 @@
 <template>
-
-
   <TheSection>
     <TheContainer>
-
-
       <UForm class="flex flex-wrap gap-4 mb-12">
-
         <UFormGroup label="Width Genres">
           <USelectMenu v-model="withGenresValue" :options="genres?.genres" option-attribute="name" valueAttribute="id"
             class="block !w-44" multiple />
@@ -17,39 +12,35 @@
             valueAttribute="id" class="block !w-44" multiple />
         </UFormGroup>
 
-
         <UFormGroup label="Type">
           <URadioGroup v-model="type" :options="typeOptions" :ui="{
             fieldset: 'flex flex-row ',
           }" :uiRadio="{
-            base: 'hidden',
-          }
-            ">
+              base: 'hidden',
+            }">
             <template #label="label">
               <UButton class="w-14" :class="getColor(label.option.value)" :label="label.option.label"
-                :icon="label.option.icon" @click="() => type = label.option.value" />
+                :icon="label.option.icon" @click="() => (type = label.option.value)" />
             </template>
           </URadioGroup>
         </UFormGroup>
       </UForm>
 
       <div>
-
         <template v-if="result.status.value === 'error'"> error </template>
         <template v-if="result.status.value === 'pending'">
           <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
-            <USkeleton class="h-24 sm:h-26 lg:h-28 w-full " v-for="item in 5" />
+            <USkeleton class="h-24 sm:h-26 lg:h-28 w-full" v-for="item in 5" />
           </div>
-
         </template>
         <template v-if="result.status.value === 'success'">
           <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
             <template v-if="type === 'movie'">
-              <MovieCard v-for="item in result.data.value?.results" :movie="(item as MovieItem)"
+              <MovieCard v-for="item in result.data.value?.results" :movie="item as MovieItem"
                 :genres="getGenres(item.genre_ids)" />
             </template>
             <template v-if="type === 'tv'">
-              <TvCard v-for="item in result.data.value?.results" :tv="(item as TvItem)"
+              <TvCard v-for="item in result.data.value?.results" :tv="item as TvItem"
                 :genres="getGenres(item.genre_ids)" />
             </template>
           </div>
@@ -59,51 +50,42 @@
       <div class="flex justify-center md:justify-start">
         <UPagination class="my-6" v-model="page" :total="500" show-first show-last />
       </div>
-
-
     </TheContainer>
   </TheSection>
-
-
-
-
 </template>
 <script setup lang="ts">
 import type { Discover, MovieItem, TvItem } from "~/types/discoverModel";
 import type { Genre } from "~/types/genreModel";
-import { typeOptions } from '~/constants'
+import { typeOptions } from "~/constants";
 const type = useRouteQuery<string>("type", "movie", {
   transform: String,
 });
 const { data: genres } = await useFetch<Genre>(`/api/v3/genre/${type.value}`);
 
-
-const withGenresValue = ref<number[]>([])
+const withGenresValue = ref<number[]>([]);
 const withGenresComputed = computed(() => {
-  let temp = ''
+  let temp = "";
   withGenresValue.value.forEach((item) => {
-    temp += `${item},`
-  })
-  return temp
-})
-watch(withGenresComputed, () => with_genres.value = withGenresComputed.value)
-const with_genres = useRouteQuery<string>("with_genres", '');
+    temp += `${item},`;
+  });
+  return temp;
+});
+watch(withGenresComputed, () => (with_genres.value = withGenresComputed.value));
+const with_genres = useRouteQuery<string>("with_genres", "");
 
-
-
-const withoutGenresValue = ref<number[]>([])
+const withoutGenresValue = ref<number[]>([]);
 const withoutGenresComputed = computed(() => {
-  let temp = ''
+  let temp = "";
   withoutGenresValue.value.forEach((item) => {
-    temp += `${item},`
-  })
-  return temp
-})
-watch(withoutGenresComputed, () => without_genres.value = withoutGenresComputed.value)
-const without_genres = useRouteQuery<string>("without_genres", '');
-
-
-
+    temp += `${item},`;
+  });
+  return temp;
+});
+watch(
+  withoutGenresComputed,
+  () => (without_genres.value = withoutGenresComputed.value)
+);
+const without_genres = useRouteQuery<string>("without_genres", "");
 
 const page = useRouteQuery("page", "1", {
   transform: Number,
@@ -138,8 +120,8 @@ const getGenres = (ids: number[]) => {
 };
 
 const getColor = (value: string) => {
-  return type.value == value ? '!bg-primary-500' : '!bg-primary-200'
-}
+  return type.value == value ? "!bg-primary-500" : "!bg-primary-200";
+};
 </script>
 <style>
 .loader {
@@ -150,7 +132,9 @@ const getColor = (value: string) => {
   border-top: 0;
   box-sizing: border-box;
   background:
-    radial-gradient(farthest-side at top, #0000 calc(100% - 5px), #e7ef9d calc(100% - 4px)),
+    radial-gradient(farthest-side at top,
+      #0000 calc(100% - 5px),
+      #e7ef9d calc(100% - 4px)),
     radial-gradient(2px 3px, #5c4037 89%, #0000) 0 0/17px 12px,
     #ff1643;
   --c: radial-gradient(farthest-side, #000 94%, #0000);
@@ -179,32 +163,81 @@ const getColor = (value: string) => {
 
 @keyframes l8 {
   0% {
-    -webkit-mask-size: auto, 0 0, 0 0, 0 0, 0 0, 0 0, 0 0
+    -webkit-mask-size:
+      auto,
+      0 0,
+      0 0,
+      0 0,
+      0 0,
+      0 0,
+      0 0;
   }
 
   15% {
-    -webkit-mask-size: auto, 20px 20px, 0 0, 0 0, 0 0, 0 0, 0 0
+    -webkit-mask-size:
+      auto,
+      20px 20px,
+      0 0,
+      0 0,
+      0 0,
+      0 0,
+      0 0;
   }
 
   30% {
-    -webkit-mask-size: auto, 20px 20px, 20px 20px, 0 0, 0 0, 0 0, 0 0
+    -webkit-mask-size:
+      auto,
+      20px 20px,
+      20px 20px,
+      0 0,
+      0 0,
+      0 0,
+      0 0;
   }
 
   45% {
-    -webkit-mask-size: auto, 20px 20px, 20px 20px, 20px 20px, 0 0, 0 0, 0 0
+    -webkit-mask-size:
+      auto,
+      20px 20px,
+      20px 20px,
+      20px 20px,
+      0 0,
+      0 0,
+      0 0;
   }
 
   60% {
-    -webkit-mask-size: auto, 20px 20px, 20px 20px, 20px 20px, 20px 20px, 0 0, 0 0
+    -webkit-mask-size:
+      auto,
+      20px 20px,
+      20px 20px,
+      20px 20px,
+      20px 20px,
+      0 0,
+      0 0;
   }
 
   75% {
-    -webkit-mask-size: auto, 20px 20px, 20px 20px, 20px 20px, 20px 20px, 20px 20px, 0 0
+    -webkit-mask-size:
+      auto,
+      20px 20px,
+      20px 20px,
+      20px 20px,
+      20px 20px,
+      20px 20px,
+      0 0;
   }
 
   90%,
   100% {
-    -webkit-mask-size: auto, 20px 20px, 20px 20px, 20px 20px, 20px 20px, 20px 20px, 20px 20px
+    -webkit-mask-size:
+      auto,
+      20px 20px,
+      20px 20px,
+      20px 20px,
+      20px 20px,
+      20px 20px,
+      20px 20px;
   }
 }
 </style>
