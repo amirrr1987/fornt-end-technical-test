7862613c1961ebae7731fb2ed75fcd11375998be
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-06 11:13:24 +0330

    Update index.ts

--- server/api/v3/configuration/index.ts
+++ server/api/v3/configuration/index.ts
@@ -1,4 +1,6 @@
+import type { Configuration } from "~/types/configurationModel";
+
 export default defineEventHandler(async () => {
   const url = `https://api.themoviedb.org/3/configuration?api_key=55ee9c566996339d9859d1ec68533e20`;
-  return await $fetch(url);
+  return await $fetch<Configuration>(url);
 });
