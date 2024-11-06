3bf7a21f7dbc74bffb4f5b6b63453593300ecc46
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-06 12:39:04 +0330

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <slot />
+  <nav>
+    <TheContainer>
+      <NuxtLink to="/">The Movie</NuxtLink>
+    </TheContainer>
+  </nav>
+  <main>
+    <slot />
+  </main>
 </template>
 
 <style scoped></style>
